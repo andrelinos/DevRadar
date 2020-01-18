8312af1bed9eae417fcb7a5e441aad2d55aed051
Andrelino Silva <35929943+andrelinos@users.noreply.github.com>
2020-01-18 16:57:42 -0300

    Add files via upload

---
+++ mobile/src/pages/Main.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Image, View, Text } from 'react-native';
+import MapView, { Marker, Callout } from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
+
+function Main() {
+    const [currentRegion, setCurrentRegion] = useState(null);
+
+    useEffect(() => {
+        async function loadInitialPosition(){
+         const { granted } = await requestPermissionsAsync();
+
+         if (granted) {
+             const { coords } = await getCurrentPositionAsync({
+                 enableHighAccuracy: true,
+             });
+
+             const { latitude, longitude } = coords;
+
+             setCurrentRegion({
+                 latitude,
+                 longitude,
+                 latitudeDelta: 0.04,
+                 longitudeDelta: 0.04,
+             })
+         }
+    }
+            
+        loadInitialPosition();
+    }, []);
+
+    if(!currentRegion) {
+        return null;
+    }
+
+
+
+    return(
+    <MapView initialRegion={currentRegion} style={styles.map}>
+        <Marker coordinate={{ latitude: -20.7351466 , longitude: -42.0151267 }}>   
+            <Image style={styles.avatar} source={{ uri: 'https://avatars0.githubusercontent.com/u/35929943?s=460&v=4'}}/>
+            <Callout>
+                <View style={styles.callout}>
+                    <Text style={styles.devName}>Andrelino Silva</Text>
+                    <Text style={styles.devBio}>Qualquer coisa em Qualquer lugar</Text>
+                    <Text style={styles.devTechs}>ReactJs, React Native, Node.js</Text>
+                </View>
+            </Callout>
+        </Marker>
+    </MapView>)
+}
+
+const styles = StyleSheet.create({
+    map: {
+        flex: 1
+    },
+
+    avatar: {
+        width: 54,
+        height: 54,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#fff'
+    },
+
+    callout: {
+        width: 260,
+        borderRadius: 4
+    },
+
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    
+    devBio: {
+        color: '#999',
+        marginTop: 5,
+    },
+
+    devTechs: {
+        marginTop: 5
+    }
+})
+
+export default Main;
