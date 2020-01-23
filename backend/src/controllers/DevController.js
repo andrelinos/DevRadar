fc19df46c63ac10a9703417d823afe89e3434b07
Andrelino Silva <35929943+andrelinos@users.noreply.github.com>
2020-01-23 01:25:19 -0300

    Update DevController.js

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,8 +1,8 @@
-const axios = require('axios');
-const Dev = require('../models/Dev');
-const parseStringAsArray = require('../utils/parseStringAsArray');
-const { findConnections, sendMessage } = require('../websocket');
+import axios from 'axios';
+import Dev from '../models/Dev';
+import parseStringAsArray from '../utils/parseStringAsArray';
 
+import * as WebSocket from '../websocket';
 
 module.exports = {
   async index(request, response) {
@@ -11,87 +11,95 @@ module.exports = {
     return response.json(devs);
   },
 
+  async store(request, response) {
+    const { github_username, techs, latitude, longitude } = request.body;
 
-
-async store (request, response) {
-  const { github_username, techs, latitude, longitude } = request.body;
-
-   const dev = await Dev.findOne({ github_username });
+    const dev = await Dev.findOne({ github_username });
 
     if (!dev) {
       try {
         // aguarda uma resposta antes de prosseguir
         const apiResponse = await axios
           .get(`https://api.github.com/users/${github_username}`)
-          .catch(error => {
-        // Se o nome de usário não existe, apresenta o erro abaixo.
+          .catch(_error => {
+            // Se o nome de usário não existe, apresenta o erro abaixo.
 
-          return response.status(404).json({ error: 'User does not exist on github.'});
+            return response
+              .status(404)
+              .json({ error: 'User does not exist on github.' });
+          });
+
+        if (apiResponse.error) {
+          return response.json(apiResponse);
+        }
+
+        const { name = login, avatar_url, bio } = apiResponse.data;
+
+        const techsArray = parseStringAsArray(techs);
+
+        const location = {
+          type: 'Point',
+          coordinates: [longitude, latitude],
+        };
+
+        const dev = await Dev.create({
+          github_username,
+          name,
+          avatar_url,
+          bio,
+          techs: techsArray,
+          location,
         });
 
-      if (apiResponse.error) {
-        return response.json(apiResponse);
+        // Filtrar conexões de websocket e procurar aquelas que estão a 10km máximo
+        // Que possuem pelo menos uma tech das filtradas
+
+        const sendSocketMessageTo = WebSocket.findConnections(
+          { latitude, longitude },
+          techsArray
+        );
+
+        WebSocket.sendMessage(sendSocketMessageTo, 'new-dev', dev);
+
+        return response.json([
+          github_username,
+          name,
+          techs,
+          bio,
+          latitude,
+          longitude,
+        ]);
+      } catch (error) {
+        return response
+          .status(400)
+          .json({ error: `Something did not work as expected.${error}` });
       }
-
-      const { name = login, avatar_url, bio } = apiResponse.data;
-
-
-      const techsArray = parseStringAsArray(techs);
-
-      const location = {
-        type: 'Point',
-        coordinates: [longitude, latitude],
-      };
-
-      const dev = await Dev.create({
-        github_username,
-        name,
-        avatar_url,
-        bio,
-        techs: techsArray,
-        location,
-      });
-
-      // Filtrar conexões de websocket e procurar aquelas que estão a 10km máximo
-      // Que possuem pelo menos uma tech das filtradas
-
-      const sendSocketMessageTo = findConnections(
-        { latitude, longitude },
-        techsArray,
-      );
-
-      sendMessage(sendSocketMessageTo, 'new-dev', dev);
-
-      return response.json([
-        github_username,
-        name,
-        techs,
-        latitude,
-        longitude,
-      ]);
-    } catch (error) {
-    return response.status(400).json({ error: 'Something did not work as expected.' })
+    } else {
+      return response
+        .status(400)
+        .json({ error: 'Developer already registered in DevRadar.' });
     }
-    }else {
-      return response.status(400).json({ error: 'Developer already registered in DevRadar.' });
+  },
+
+  async update(request, response) {
+    const { id } = request.params;
+    const { github_username, techs, latitude, longitude } = request.body;
+
+    const dev = await Dev.findById(id);
+
+    if (!dev) {
+      return response.status(400).json({ error: 'Developer not found.' });
     }
 
-},
+    // aguarda uma resposta antes de prosseguir
+    const apiResponse = await axios
+      .get(`https://api.github.com/users/${github_username}`)
+      .catch(error => {
+        // Se o nome de usário não existe, apresenta o erro abaixo.
 
-async update(request, response) {
-  const { github_username, techs, latitude, longitude } = request.body;
-
-  const dev = await Dev.findById(request.params.id);
-
-  if (dev) {
-    try {
-      // aguarda uma resposta antes de prosseguir
-      const apiResponse = await axios
-        .get(`https://api.github.com/users/${github_username}`)
-        .catch(error => {
-      // Se o nome de usário não existe, apresenta o erro abaixo.
-
-        return response.status(404).json({ error: 'User does not exist on github.'});
+        return response
+          .status(404)
+          .json({ error: 'User does not exist on github.' });
       });
 
     if (apiResponse.error) {
@@ -100,7 +108,6 @@ async update(request, response) {
 
     const { name = login, avatar_url, bio } = apiResponse.data;
 
-
     const techsArray = parseStringAsArray(techs);
 
     const location = {
@@ -108,22 +115,21 @@ async update(request, response) {
       coordinates: [longitude, latitude],
     };
 
-
-    const dev = await Dev.updateOne({
+    const devUpdated = await dev.updateOne({
       github_username,
-      name,
       techs: techsArray,
-      avatar_url,
       location,
+      avatar_url,
+      name,
       bio,
     });
 
-    const sendSocketMessageTo = findConnections(
+    const sendSocketMessageTo = WebSocket.findConnections(
       { latitude, longitude },
-      techsArray,
+      techsArray
     );
 
-    sendMessage(sendSocketMessageTo, 'new-dev', dev);
+    WebSocket.sendMessage(sendSocketMessageTo, 'new-dev', dev);
 
     return response.json([
       github_username,
@@ -132,35 +138,16 @@ async update(request, response) {
       avatar_url,
       name,
       bio,
-    ] );
+    ]);
+  },
 
+  async delete(request, response) {
+    const { id } = request.params;
 
-  } catch (error) {
+    const dev = await Dev.findById(id);
 
-  return response.status(400).json({ error: 'Something did not work as expected.' })
-  }
-  }else {
-    return response.status(400).json({ error: 'Developer already registered in DevRadar.' });
-  }
+    await dev.delete();
 
-
-},
-
-async delete(request, response) {
-
-
-  try {
-    dev = await Dev.findById(request.params.id);
-    if (!dev) return response.status(400).json({ error: 'Developer not found.' });
-
-    await dev.remove();
-
-    return response.status(200).json({ error: 'Developer removed sucess.' });
-
-  } catch (e) {
-    return response.status(400).json({ error: 'Something did not work as expected.' });
-  }
-
-},
-
+    return response.status(200).json({ message: 'Developer deleted success.' });
+  },
 };
